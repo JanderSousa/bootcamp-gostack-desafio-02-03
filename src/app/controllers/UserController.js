75252235c8b8689a3323a81a7feaf62f00af4199
Janderley Sousa <jsousa7489@gmail.com>
2019-08-29 22:44:10 -0300

    chore: Validação e alteração do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
-  async storage(req, res) {
+  async store(req, res) {
+    const Schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await Schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
+
     const { email } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -12,6 +27,45 @@ class UserController {
 
     const { id, name, provider } = await User.create(req.body);
     return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    const Schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await Schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
+
+    const { oldPassword, email } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists) {
+        return res.status(400).json({ error: 'Usuário já existe' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Senha não confere' });
+    }
+
+    const { id, name, email: userEmail } = await user.update(req.body);
+
+    return res.json({ id, name, email: userEmail });
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e3129a086d2924bfed2c4a79f703eacc',
+  expireIn: '7d',
+};
